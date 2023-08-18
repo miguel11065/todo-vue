e6b00c461bb112561f7df126a356adcc1686d85f
Miguel Leal <miguel11065@gmail.com>
2023-08-18 14:37:19 -0300

    corrigindo pequenos detalhes

--- src/components/Cabecalho.vue
+++ src/components/Cabecalho.vue
@@ -5,8 +5,9 @@
 <template>
     <header class="p-5 mb-4 mt-4 bg-light rounded-3">
         <h1>Minhas Tarefas</h1>
-        <p>
+        <p v-if="props.tarefasPendentes > 0">
         Você possui {{ props.tarefasPendentes }} tarefas pendentes
         </p>
+        <span v-else>Você não possui tarefas pendentes</span>
     </header>
 </template>
